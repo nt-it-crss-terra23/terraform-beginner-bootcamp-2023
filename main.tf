bff21abea669050dbe7b57b8cc081b601ed68259
Norbert T <ntoth.it.crss+gith.terra23@protonmail.ch>
2023-10-05 00:30:32 +0000

    #13 migrate local state to terraform cloud using terraform cloud block

--- main.tf
+++ main.tf
@@ -1,4 +1,18 @@
 terraform {
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "GalacticEmpireNT"
+
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+  cloud {
+    organization = "GalacticEmpireNT"
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
   required_providers {
     random = {
       source = "hashicorp/random"
